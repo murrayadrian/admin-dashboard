0c4db8f7088f795ad128ce0052ef795587d995ec
murrayadrian <vinhduongtrung812@outlook.com>
2023-06-01 13:28:06 +0700

    add export excel

--- src/pages/Customer/ExportCustomer.js
+++ src/pages/Customer/ExportCustomer.js
@@ -1,7 +1,49 @@
+import Excel from "components/Excel"
+import api from 'api/config'
+import { useEffect, useState } from "react"
+import { Button } from "antd"
+
+
 export const ExportCustomer = () => {
+  const [ customers, setCustomers] = useState([])
+  useEffect(()=>{
+    const getCustomers = async() =>{
+      const res = await api.get("/customers")
+      setCustomers(res.data)
+    }
+    getCustomers()
+  },[])
+  const data=[
+    {
+      columns: [
+        {
+          title: "Customer Id",
+          dataIndex: "id",
+          width: 10,
+        },
+        {
+          title: "Name",
+          dataIndex: "name",
+          width: 20,
+        },
+        {
+          title: "Age",
+          dataIndex: "age",
+          width: 30,
+        },
+      ],
+      data: customers,
+      tabName: "info",
+    }
+  ]
     return (
         <div>
-          Export Customer
+          {customers.length === 0 ? <h2>No customers</h2>:
+          <Excel fileName="export-customers" data={data}>
+            <Button>Export customers</Button>
+          </Excel>
+          }
         </div>
     )
 }
+
